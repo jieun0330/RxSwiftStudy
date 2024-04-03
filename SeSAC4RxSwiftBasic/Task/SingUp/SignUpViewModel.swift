ee62ad9f9e9dd82a5f7271f91905d82e7a93adc9
jieun <jieun_0330@naver.com>
2024-04-03 23:36:21 +0900

    SignUp화면 ViewModel 구현

---
+++ SeSAC4RxSwiftBasic/Task/SingUp/SignUpViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewModel.swift
+//  SeSAC4RxSwiftBasic
+//
+//  Created by 박지은 on 4/3/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class SignUpViewModel {
+    let sampleEmail = BehaviorRelay(value: "a@a.com")
+}
--- SeSAC4RxSwiftBasic/Task/SingUp/SingUpViewController.swift
+++ SeSAC4RxSwiftBasic/Task/SingUp/SingUpViewController.swift
@@ -10,58 +10,85 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-final class SignUpViewController: UIViewController {
-
-    private let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
-    private let validationButton = UIButton()
-    private let nextButton = PointButton(title: "다음")
-    private let disposeBag = DisposeBag()
+final class SignUpViewController: BaseViewController {
     
-//    private let email = "a@a.com"
-//    private let sampleEmail = Observable.just("a@a.com")
+    private let viewModel = SignUpViewModel()
+    
+    private let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
+    private lazy var validationButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("중복확인", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(validationButtonClicked), for: .touchUpInside)
+        return button
+    }()
+    private let nextButton = PointButton(title: "다음")
     private let buttonColor = Observable.just(UIColor.blue)
-    private let sampleEmail = BehaviorSubject(value: "a@a.com") // subject는 이벤트를 받을 수도 있다
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        bind()
+    }
+    
+    @objc private func validationButtonClicked() {
+        print(#function)
+    }
+    
+    override func configureHierarchy() {
+        [emailTextField, validationButton, nextButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func configureConstraints() {
+        
+        validationButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.width.equalTo(100)
+        }
+        
+        emailTextField.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.trailing.equalTo(validationButton.snp.leading).offset(-8)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.top.equalTo(emailTextField.snp.bottom).offset(30)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    
+    override func configureView() {
+        
         view.backgroundColor = .white
-        
-        configureLayout()
-        configure()
-        
-//        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+    }
+    
+    private func bind() {
         
         nextButton.rx.tap
             .bind(with: self) { owner, _ in
                 owner.navigationController?.pushViewController(PasswordViewController(), animated: true)
             }
             .disposed(by: disposeBag)
-
-//        emailTextField.rx.text.orEmpty
-//            .bind(with: self) { owner, value in
-//                owner.emailTextField.text = value
-//            }
-//            .disposed(by: disposeBag)
         
-        sampleEmail
-            .bind(with: self) { owner, value in
-                owner.emailTextField.text = value
-            }
+        viewModel.sampleEmail
+            .asDriver()
+            .drive(emailTextField.rx.text)
             .disposed(by: disposeBag)
         
-        // sampleEmail -> 바로 TextField로 보내기
-        sampleEmail
-            .bind(to: emailTextField.rx.text)
-            .disposed(by: disposeBag)
-          
         validationButton.rx.tap
             .bind(with: self) { owner, _ in
-//                owner.emailTextField.text = "b@b.com"
-//                owner.sampleEmail.bind(to: "b@b.com")
-                // = 로 값을 바꾸지 않는다
-                owner.sampleEmail.onNext("b@b.com")
-                // Observable은 이벤트를 보내기만 하지, 받을 순 없다 -> 그래서 subject 개념이 등장 -> 위에 BehaviorSubject
+                owner.viewModel.sampleEmail.accept("b@b.com")
             }
             .disposed(by: disposeBag)
         
@@ -75,40 +102,5 @@ final class SignUpViewController: UIViewController {
             .map { $0.cgColor }
             .bind(to: emailTextField.layer.rx.borderColor)
             .disposed(by: disposeBag)
-    }
-    
-
-    func configure() {
-        validationButton.setTitle("중복확인", for: .normal)
-        validationButton.setTitleColor(.black, for: .normal)
-        validationButton.layer.borderWidth = 1
-        validationButton.layer.borderColor = UIColor.black.cgColor
-        validationButton.layer.cornerRadius = 10
-    }
-    
-    func configureLayout() {
-        view.addSubview(emailTextField)
-        view.addSubview(validationButton)
-        view.addSubview(nextButton)
-        
-        validationButton.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
-            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.width.equalTo(100)
-        }
-        
-        emailTextField.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
-            make.leading.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.trailing.equalTo(validationButton.snp.leading).offset(-8)
-        }
-        
-        nextButton.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(emailTextField.snp.bottom).offset(30)
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
-        }
     }
 }
